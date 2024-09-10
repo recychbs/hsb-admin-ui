708496058074068c66fcd9ebe63c59a234adf0a8
kalaiarasan27 <140267884+kalaiarasan27@users.noreply.github.com>
2024-09-10 22:20:58 +0530

    Update User.jsx

--- src/screen/pages/User.jsx
+++ src/screen/pages/User.jsx
@@ -1,32 +1,62 @@
 import { useNavigate } from "react-router-dom";
 
 function User() {
-  const users = [
-    {
-      id: 1,
-      name: "User Id 1",
-      phone: "000-000-0000",
-      mail: "@twitter",
-      location: "place",
-      amt: "$ 400"}
-      ,
-    {
-      id: 2,
-      name: "User Id 2",
-      phone: "000-000-0000",
-      mail: "@twitter",
-      location: "place",
-      amt: "$ 400"}
-      ,
-    {
-      id: 3,
-      name: "User Id 3",
-      phone: "000-000-0000",
-      mail: "@twitter",
-      location: "place",
-      amt: "$ 400"}
-      ,
-  ];
+    const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://django-djreact-app-d5af3d4e3559.herokuapp.com/Get_UserProfile/')
+      .then(response => response.json())
+      .then(data =>{ 
+        setData(data);     
+
+
+
+        
+        setLoading(false);
+    })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+
+const users=[];
+
+  for (let i=0;i<data.length;1++){
+        users.push({
+        User_Name: data[i].user_Name,
+        mail: data[i].Email,
+        phone: data[i].Phone_Number,
+        address: data[i].Address,
+        nationality: data[i].Nationality
+        });
+  }
+  
+  // const users = [
+  //   {
+  //     id: 1,
+  //     name: "User Id 1",
+  //     phone: "000-000-0000",
+  //     mail: "@twitter",
+  //     location: "place",
+  //     amt: "$ 400"}
+  //     ,
+  //   {
+  //     id: 2,
+  //     name: "User Id 2",
+  //     phone: "000-000-0000",
+  //     mail: "@twitter",
+  //     location: "place",
+  //     amt: "$ 400"}
+  //     ,
+  //   {
+  //     id: 3,
+  //     name: "User Id 3",
+  //     phone: "000-000-0000",
+  //     mail: "@twitter",
+  //     location: "place",
+  //     amt: "$ 400"}
+  //     ,
+  // ];
 
   const navigate = useNavigate();
 
