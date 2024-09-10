664d346cde1fd5aadf6440f1b776d4defb44c312
kalaiarasan27 <140267884+kalaiarasan27@users.noreply.github.com>
2024-09-10 22:24:14 +0530

    Update Userdetail.jsx

--- src/screen/pages/Userdetail.jsx
+++ src/screen/pages/Userdetail.jsx
@@ -12,11 +12,11 @@ function Userdetail() {
         <div className="container-fluid" style={{flexDirection:"column"}}>
            <h3 style={{fontWeight:"bold"}}>user Details</h3>
             <p><strong>ID:</strong> {user.id}</p>
-            <p><strong>Order:</strong> {user.name}</p>
+            <p><strong>Order:</strong> {user.user_Name}</p>
             <p><strong>Phone:</strong> {user.phone}</p>
             <p><strong>Mail:</strong> {user.mail}</p>
-            <p><strong>Location:</strong> {user.location}</p>
-            <p><strong>Amount:</strong> {user.amt}</p>     
+            <p><strong>Address:</strong> {user.address}</p>
+            <p><strong>Nationality:</strong> {user.nationality}</p>     
         </div>
     );
 }
